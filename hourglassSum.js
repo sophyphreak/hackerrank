53a005475773af235cb5ade5c5de405c5b947dd2
Andrew <arhorn@smcm.edu>
2019-03-04 16:54:27 -0600

    finished hourglassSum problem

---
+++ hourglassSum.js
@@ -0,0 +1,52 @@
+const hourglassSum = arr => {
+  let maxHourglassSum;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const currentSum =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      if (currentSum > maxHourglassSum || maxHourglassSum === undefined) maxHourglassSum = currentSum;
+    }
+  }
+  return maxHourglassSum;
+};
+
+const assert = require('assert');
+assert.equal(
+  hourglassSum([
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+  ]),
+  7
+);
+assert.equal(
+  hourglassSum([
+    [-9, -9, -9, 1, 1, 1],
+    [0, -9, 0, 4, 3, 2],
+    [-9, -9, -9, 1, 2, 3],
+    [0, 0, 8, 6, 6, 0],
+    [0, 0, 0, -2, 0, 0],
+    [0, 0, 1, 2, 4, 0]
+  ]),
+  28
+);
+assert.equal(
+  hourglassSum([
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 2, 4, 4, 0],
+    [0, 0, 0, 2, 0, 0],
+    [0, 0, 1, 2, 4, 0]
+  ]),
+  19
+);
