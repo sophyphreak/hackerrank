822ca352d667fab537c555dc0347353caf62a370
Andrew <arhorn@smcm.edu>
2019-03-04 19:17:49 -0600

    added makeAnagram solution

--- strings/makeAnagram.js
+++ strings/makeAnagram.js
@@ -1,14 +1,26 @@
 const makeAnagram = (a, b) => {
   let mustDelete = 0;
-  const aSet = new Set(a);
-  const bSet = new Set(b);
-  for (let letter of a) {
-    if (!bSet.has(letter)) {
-      mustDelete++;
+  const aSorted = a.split('').sort().join('');
+  const bSorted = b.split('').sort().join('');
+  let aIndex = 0;
+  let bIndex = 0;
+  while (true) {
+    if (aSorted[aIndex] === undefined) {
+      mustDelete += bSorted.length - bIndex;
+      break;
     }
-  }
-  for (let letter of b) {
-    if (!aSet.has(letter)) {
+    if (bSorted[bIndex] === undefined) {
+      mustDelete += aSorted.length - aIndex;
+      break;
+    }
+    if (aSorted[aIndex] === bSorted[bIndex]) {
+      aIndex++;
+      bIndex++;
+    } else if (aSorted[aIndex] < bSorted[bIndex]) {
+      aIndex++;
+      mustDelete++;
+    } else {
+      bIndex++;
       mustDelete++;
     }
   }
@@ -18,3 +30,5 @@ const makeAnagram = (a, b) => {
 const assert = require('assert');
 
 assert.equal(makeAnagram('cde', 'abc'), 4);
+assert.equal(makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'), 30);
+assert.equal(makeAnagram('showman', 'woman'), 2);
