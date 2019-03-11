0b8493e3f8acfde38dc8508d7a8b2a85dcb79e29
Andrew <arhorn@smcm.edu>
2019-03-10 23:15:36 -0500

    started minimumBribes

---
+++ arrays/minimumBribes.js
@@ -0,0 +1,8 @@
+const minimumBribes = q => {
+  
+};
+
+const assert = require('assert');
+
+assert.equal(minimumBribes([2, 1, 5, 3, 4]), 3, 'test 1');
+assert.equal(minimumBribes([2, 5, 1, 3, 4]), 'Too chaotic', 'test 2');
